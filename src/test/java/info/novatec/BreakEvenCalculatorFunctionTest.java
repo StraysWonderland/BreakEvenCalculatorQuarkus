49256cca0aec8177e3367d8fcfcc9c97a9da4aac
Cagri Tasci <cagri.c.tasci@outlook.de>
2020-02-07 10:57:27 +0100

    adjusted tests for response bean

--- src/test/java/info/novatec/BreakEvenCalculatorFunctionTest.java
+++ src/test/java/info/novatec/BreakEvenCalculatorFunctionTest.java
@@ -4,11 +4,16 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import javax.inject.Inject;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class BreakEvenCalculatorFunctionTest {
+
+    @Inject
+    BreakEvenResponse response;
 
     @ParameterizedTest
     @CsvSource({
@@ -17,14 +22,14 @@ public class BreakEvenCalculatorFunctionTest {
             "2.30, 333.33, 2.10, 1667",
             "3.00, 8000.00, 1.50, 5334"}
     )
-    public void testBreakEvenFunction(double price, double fixedCost, double unitCost, String breakEvenPoint) {
-        given()
-                .queryParam("price", price)
+    public void testBreakEvenFunction(double price, double fixedCost, double unitCost, int breakEvenPoint) {
+        response.breakEvenPoint = breakEvenPoint;
+        given().queryParam("price", price)
                 .queryParam("fixedCost", fixedCost)
                 .queryParam("unitCost", unitCost)
                 .when().get("/run")
                 .then()
                 .statusCode(200)
-                .body(is(breakEvenPoint));
+                .equals(response);
     }
 }
